df2d1805bfe3d46403dfe82e6a6de460e27414fd
tristan <tristanaaron.2711@gmail.com>
2023-11-15 08:49:51 +0700

    Update Attack Animation

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -8,6 +8,8 @@ public class ZombieMovement : MonoBehaviour
     private Animator animator;
     private Transform player;
     public float detectionRadius = 20f;
+    public float attackradius = 2f;
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +21,23 @@ public class ZombieMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.position) <= detectionRadius)
+        
+        if (Vector3.Distance(transform.position, player.position) <= attackradius)
+        {
+            animator.SetBool("isAttack", true);
+            animator.SetBool("isMoving", false);
+            //Masukin damage player ke sini
+        }
+        else if(Vector3.Distance(transform.position, player.position) <= detectionRadius)
         {
             zombie.SetDestination(player.position);
             animator.SetBool("isMoving", true);
-        }else
+            animator.SetBool("isAttack", false);
+        }
+        else
         {
-
             animator.SetBool("isMoving", false);
+            animator.SetBool("isAttack", false);
         }
 
     }
