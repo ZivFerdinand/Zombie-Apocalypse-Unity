595994acf0db1e79f9ece707e63ef6bf2893fd45
shamgare <stsidkenuu@gmail.com>
2023-11-21 07:49:04 +0700

    Added Color Change to Healthbar

--- Assets/Scripts/UI/PlayerHealth.cs
+++ Assets/Scripts/UI/PlayerHealth.cs
@@ -18,13 +18,17 @@ public class PlayerHealth : MonoBehaviour
     private float updtDmg;
 
     private float durationTimer;
-
+    public Slider healthBarSlider;
 
     void Start()
     {
         currentHealth = maxHealth;
         overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0);
         updtDmg = fadeSpeed;
+
+        healthBarSlider.maxValue = maxHealth;
+        healthBarSlider.value = maxHealth;
+        SetHealthBarColor();
     }
 
     void Update()
@@ -79,6 +83,7 @@ public class PlayerHealth : MonoBehaviour
         healthBar.SetHealth(currentHealth);
         durationTimer = 0;
         overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0.55f);
+        SetHealthBarColor();
     }
     public void healPlayer(float heal)
     {
@@ -90,5 +95,22 @@ public class PlayerHealth : MonoBehaviour
         }
 
         healthBar.SetHealth(currentHealth);
+    }
+
+    void SetHealthBarColor()
+    {
+        float normalizedHealth = currentHealth / maxHealth;
+
+        // Change color based on health percentage
+        if (normalizedHealth > 0.5f)
+        {
+            // Green to Yellow transition
+            healthBarSlider.fillRect.GetComponent<Image>().color = Color.Lerp(Color.yellow, Color.green, (normalizedHealth - 0.5f) * 2f);
+        }
+        else
+        {
+            // Yellow to Red transition
+            healthBarSlider.fillRect.GetComponent<Image>().color = Color.Lerp(Color.red, Color.yellow, normalizedHealth * 2f);
+        }
     }
 }
